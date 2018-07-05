5a580ccbf58d0de8f4fd9639efa1cf764f42636c
Alexandros Gisis <algisisuni@gmail.com>
2018-07-05 23:51:22 +0300

    Add project files.

---
+++ Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("can you even lift bro?");
+            
+
+            GradeBook book = new GradeBook();
+            book.AddGrades(80);
+            book.AddGrades(95.4f);
+            book.AddGrades(73);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+        }
+    }
+}
